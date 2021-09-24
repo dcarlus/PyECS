6230b52396a97da45642fd35a45a3d084df8813e
Denis CARLUS <denis.carlus@hotmail.fr>
2021-09-24 18:29:49 +0200

    Some minor changes in the mainwindow module

--- game/__main__.py
+++ game/__main__.py
@@ -8,7 +8,6 @@ window: MainWindow = MainWindow(800, 600, 'Crystal Shot')
 world: World = cshot.get_world()
 
 running: bool = True
-clock: pygame.time = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
@@ -18,7 +17,6 @@ while running:
             window.resize(event.size[0], event.size[1])
 
         world.run()
-        window.draw()
-        clock.tick(60)
+        window.update()
 
 MainWindow.end()
--- game/mainwindow.py
+++ game/mainwindow.py
@@ -14,6 +14,8 @@ class MainWindow:
         pygame.init()
         pygame.display.set_mode((width, height), pygame.RESIZABLE)
         pygame.display.set_caption(caption)
+        self.m_framerate: int = 60
+        self.m_clock: pygame.time = pygame.time.Clock()
 
         if icon is not None:
             pygame.display.set_icon(icon)
@@ -24,10 +26,19 @@ class MainWindow:
         print('Quit {}'.format(pygame.display.get_caption()[0]))
         pygame.quit()
 
-    def resize(self, width: int, height: int):
+    @property
+    def framerate(self) -> int:
+        return self.m_framerate
+
+    @framerate.setter
+    def framerate(self, framerate: int) -> None:
+        self.m_framerate = min(max(0, framerate), 360)
+
+    def resize(self, width: int, height: int) -> None:
         """Resize the MainWindow."""
         pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
-    def draw(self):
-        """Update the content of the MainWindow."""
+    def update(self) -> None:
+        """Update the content of the MainWindow and wait for the next frame."""
         pygame.display.flip()
+        self.m_clock.tick(self.m_framerate)
